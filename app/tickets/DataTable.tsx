aa57ff3b06ec6a7b3c5e39f33739a2ad4b76c75b
Aleksey Lovchikov <aleksey.lovchikov@gmail.com>
2024-03-14 13:11:35 -0300

    add format date time

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -14,32 +14,41 @@ interface Props {
 }
 
 const DataTable = ({ tickets }: Props) => {
-  console.log(tickets);
-
   return (
-    <div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Priority</TableHead>
-            <TableHead>Created at</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {tickets
-            ? tickets.map((ticket) => (
-                <TableRow key={ticket.id} data-href="/">
-                  <TableCell>{ticket.title}</TableCell>
-                  <TableCell>{ticket.status}</TableCell>
-                  <TableCell>{ticket.priority}</TableCell>
-                  <TableCell>{ticket.createdAt.toLocaleDateString()}</TableCell>
-                </TableRow>
-              ))
-            : null}
-        </TableBody>
-      </Table>
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>Created at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-href="/">
+                    <TableCell>{ticket.title}</TableCell>
+                    <TableCell>{ticket.status}</TableCell>
+                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString("ru-RU", {
+                        year: "2-digit",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "2-digit",
+                        hour12: false,
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };

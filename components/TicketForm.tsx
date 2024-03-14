51f6ee1c5bd5a8b23291c5be811d86ee0a79a530
Aleksey Lovchikov <svdmusic@gmail.com>
2024-03-14 15:34:20 -0300

    add react hook form

--- components/TicketForm.tsx
+++ components/TicketForm.tsx
@@ -1,9 +1,52 @@
 "use client";
 
 import React from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { z } from "zod";
+import { ticketSchema } from "@/ValidationSchemas/ticket";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import SimpleMdeReact from "react-simplemde-editor";
+
+type TicketFormData = z.infer<typeof ticketSchema>;
 
 const TicketForm = () => {
-  return <div>TicketForm</div>;
+  const form = useForm<TicketFormData>({
+    resolver: zodResolver(ticketSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof ticketSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ticket title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Ticket title..." {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Controller
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <SimpleMdeReact placeholder="Description" {...field} />
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default TicketForm;

965e225fba7c6baf4692d542882ff1c3974d8b9f
Aleksey Lovchikov <lovchikov.a3@wb.ru>
2024-04-20 22:05:00 -0300

    fix 404

--- components/NotFoundText.tsx
+++ components/NotFoundText.tsx
@@ -3,8 +3,9 @@ import React from "react";
 const NotFoundText = () => {
   return (
     <div className="flex items-center justify-center">
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p className="text-center">
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
     </div>
   );
 };

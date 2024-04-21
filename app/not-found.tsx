7ba5314a7ea7b70a2a8a023d5f3a844c4fc196cf
Aleksey Lovchikov <lovchikov.a3@wb.ru>
2024-04-20 22:01:58 -0300

    add not found

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+import NotFoundText from "@/components/NotFoundText";
+import React from "react";
+
+const NotFound = () => {
+  return <NotFoundText />;
+};
+
+export default NotFound;
---
+++ components/NotFoundText.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundText = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  );
+};
+
+export default NotFoundText;
